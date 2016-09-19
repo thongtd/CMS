097653c9d0ce6bae3779cc514e0377ed252e4435
DESKTOP-12R76KJ\thongtd <humg.thongit@gmail.com>
2016-09-20 00:08:56 +0700

    For login dashboard

---
+++ Dashboard/Controllers/AuthBaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dashboard.Controllers
+{
+    public class AuthBaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //var user = HttpContext.Session.TryGetValue("Admin.UserModel");
+            //if (user == null)
+            //    filterContext.Result = RedirectToAction("Login", "Auth", new { area = "Authen", returnUrl = Request.Url.LocalPath });
+            //else
+            //    base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Dashboard/Controllers/AuthenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dashboard.Controllers
+{
+    [Route("authen")]
+    public class AuthenController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginModel model, string returnUrl)
+        {
+            return View();
+        }
+    }
+}
