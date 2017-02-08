2cdbdf764407dbad0822ca2401b175535413de42
DESKTOP-12R76KJ\thongtd <humg.thongit@gmail.com>
2017-02-08 22:25:17 +0700

    Add FlatShop Area

---
+++ CMS.Dashboard/Areas/FlatShop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CMS.Dashboard.Areas.FlatShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMS.Dashboard/Areas/FlatShop/FlatShopAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace CMS.Dashboard.Areas.FlatShop
+{
+    public class FlatShopAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "FlatShop";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Root",
+                "",
+                new { controller = "Home", action = "Index" },
+                new[] { "CMS.Dashboard.Areas.FlatShop.Controllers" }
+            );
+
+            context.MapRoute(
+                "FlatShop_default",
+                "FlatShop/{controller}/{action}/{id}",
+                new { conntroller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "CMS.Dashboard.Areas.FlatShop.Controllers" }
+            );
+        }
+    }
+}
